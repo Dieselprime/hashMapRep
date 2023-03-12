99fed13248575f81b6ec01676deb38559d022a6e
Dieselprime <zzxsu@mail.ru>
2023-03-12 14:16:37 +0300

    inital commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
+
+
+//    Напишите метод, который получает на вход Map<K, V> и возвращает Map, где ключи и значения поменяны местами. Так как значения могут совпадать, то тип значения в Map будет уже не K, а
+//        Collection<K>:
+//
+//        Map<V, Collection<K>>
