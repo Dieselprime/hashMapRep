0f5ea364d2f26c0929118b6ff5aa3ba724fa7487
Dieselprime <zzxsu@mail.ru>
2023-03-12 15:18:12 +0300

    (added mapRev method)

--- src/HashMapReverse.java
+++ src/HashMapReverse.java
@@ -1,6 +1,24 @@
-public class HashMapReverse<K,V> {
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HashMapReverse<K, V> {
 
     public HashMapReverse() {
     }
 
+    public Map<V, Collection<K>> mapRev(Map<K, V> hashM) {
+
+        Map<V, Collection<K>> hashMaRrev = new HashMap<>();
+
+       // hashM.keySet(); // получаем ключи
+        for (K k : hashM.keySet()) {
+            hashM.get(k); // получаем значения
+
+            hashMaRrev.put(hashM.get(k), List.of(k));
+        }
+
+        return hashMaRrev;
+    }
 }
