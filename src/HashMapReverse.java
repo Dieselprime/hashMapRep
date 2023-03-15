d468f5f1ff7b004691e6fd0ffe9c69622a02e8c4
Dieselprime <zzxsu@mail.ru>
2023-03-15 19:29:04 +0300

    (added возможное решение)

--- src/HashMapReverse.java
+++ src/HashMapReverse.java
@@ -5,20 +5,19 @@ import java.util.Map;
 
 public class HashMapReverse<K, V> {
 
-    public HashMapReverse() {
-    }
-
     public Map<V, Collection<K>> mapRev(Map<K, V> hashM) {
 
-        Map<V, Collection<K>> hashMaRrev = new HashMap<>();
+        Map<V, Collection<K>> hashMapRev = new HashMap<>();
 
        // hashM.keySet(); // получаем ключи
         for (K k : hashM.keySet()) {
             hashM.get(k); // получаем значения
-
-            hashMaRrev.put(hashM.get(k), List.of(k));
+            if (hashM.get(k)==) { // если есть одинаковые значения, выводить их все, а не одно из.
+ // помещать все значения в пустой массив, а затем вводить его
+            }
+            hashMapRev.put(hashM.get(k), List.of(k));
         }
 
-        return hashMaRrev;
+        return hashMapRev;
     }
 }
